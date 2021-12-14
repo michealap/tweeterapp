e04694b14afc610ee3a74fea837569ca84e3e463
Micheala Palmer <michealapalmer@gmail.com>
2021-12-14 18:56:42 -0500

    implement the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  console.log("DOM is ready!");
+  const $newTweetCount = $('#tweet-text');
+  $newTweetCount.on('input', function() {
+    let currentCount = 140;
+    const counter = $(this).closest('.new-tweet').find('.counter');
+    //closest goes up the tree to parent node then finds counter
+    //find method returns an array with one element
+    currentCount -= $(this).val().length; //you can also use this.value.length to update the current counter
+    counter[0].value = currentCount;
+    //refers to tweet text
+    if (currentCount >= 0) {
+      counter.removeClass('red');
+    }
+    if (currentCount < 0) {
+      counter.addClass('red');
+    }
+  });
+});
+
